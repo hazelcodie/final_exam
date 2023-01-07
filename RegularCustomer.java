68ef24c077ac824b2a9425b49268841abaf59a31
hazelcodie <113188629+hazelcodie@users.noreply.github.com>
2023-01-07 16:25:34 +0800

    Add files via upload

---
+++ RegularCustomer.java
@@ -0,0 +1,15 @@
+
+public class RegularCustomer extends Customer {
+
+	public RegularCustomer(String name, double amount) {
+		super(name, amount);
+	}
+
+	@Override
+	public double calculateBill() {
+		return amount;
+	}
+
+}
+
+
